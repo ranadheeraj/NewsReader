7525483aca9c204a8cc3bcebcccdfe893a4b5c1c
w3schools-test <test@w3schools.com>
2023-11-10 20:32:32 +0000

    dark mode

--- app/src/main/java/com/example/newsreader/MainActivity.kt
+++ app/src/main/java/com/example/newsreader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newsreader
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,7 +9,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,11 +49,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Testing()
-{
-
-}
+@Preview(name ="Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground =true,
+    name="Dark Mode"
+)
 @Preview
 @Composable
 fun NewsReaderApp(){
@@ -73,14 +79,27 @@ fun NewsReaderimage(modifier:Modifier =Modifier){
         modifier =modifier,
         horizontalAlignment =Alignment.CenterHorizontally
     ){
+        Text(
+            text="Rana Dheeraj",
+            color =MaterialTheme.colorScheme.secondary,
+            style=MaterialTheme.typography.titleSmall
+        )
         Image(
             painter =painterResource(image),
-            contentDescription =result.toString()
+            contentDescription =result.toString(),
+            modifier =Modifier.size(1000.dp).clip(CircleShape)
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick ={result =(1..3).random()}){
             Text(stringResource(R.string.start))
         }
-    }
 
-}
+    }
+     Surface(shape=MaterialTheme.shapes.medium, shadowElevation = 1.dp){
+     Text(
+         text="end note",
+         modifier =Modifier.padding(all =4.dp),
+         style =MaterialTheme.typography.bodyMedium
+
+     )
+}}
